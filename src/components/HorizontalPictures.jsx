f2f0bbe3cad618cce782fa290255758a00b1bcf4
SKIPAH <juzzii.mehta@gmail.com>
2024-10-10 14:57:17 +0300

    New images and testing js stuff

---
+++ src/components/HorizontalPictures.jsx
@@ -0,0 +1,45 @@
+export default function HorizontalPictures() {
+  return (
+    <>
+      <div className="gallery-wrap">
+        <img src="" id="backButton"></img>
+        <img src="" id="nextButton"></img>
+      </div>
+      <div className="gallery">
+        <span>
+          <img
+            className="widerpic"
+            src="src/files/images/nyyrikkipictures/assembly.jpg"
+          ></img>
+        </span>
+        <span>
+          <img
+            className="widerpic"
+            src="src/files/images/nyyrikkipictures/ebinsda.jpg"
+          ></img>
+        </span>
+        <span>
+          <img src="src/files/images/nyyrikkipictures/nyyrikkiskipah.jpg"></img>
+        </span>
+        <span>
+          <img src="src/files/images/nyyrikkipictures/nyyrikkiskipah2.png"></img>
+        </span>
+        <span>
+          <img src="src/files/images/nyyrikkipictures/skipahuusi.jpeg"></img>
+        </span>
+        <span>
+          <img
+            className="widerpic"
+            src="src/files/images/nyyrikkipictures/skipah3.JPG"
+          ></img>
+        </span>
+        <span>
+          <img
+            className="widerpic"
+            src="src/files/images/nyyrikkipictures/skipah4.JPG"
+          ></img>
+        </span>
+      </div>
+    </>
+  );
+}
