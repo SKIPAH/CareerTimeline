876f27216ebc27c3ba07a6fbc33443effcb01084
SKIPAH <juzzii.mehta@gmail.com>
2024-10-24 17:20:54 +0300

    small additions after learning html basics

--- src/Pages/DbzPage.jsx
+++ src/Pages/DbzPage.jsx
@@ -6,7 +6,7 @@ export default function DbzPage() {
     <>
       <section className="dbz-section">
         <div className="dbz-container">
-          <h1 className="title-text">TEST PAGE FOR NOW</h1>
+          <h1 className="title-text">TEST PAGE FOR NOW 	&#128511; &#128018;</h1>
           <p className="dbz-text"></p>
         </div>
         <BackgroundVideo></BackgroundVideo>
