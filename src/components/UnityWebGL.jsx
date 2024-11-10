eff86699f2a50ddb64c00ddfe906ca46e55ea6fe
SKIPAH <juzzii.mehta@gmail.com>
2024-11-10 23:39:56 +0200

    unity disabled on mobile

--- src/components/UnityWebGL.jsx
+++ src/components/UnityWebGL.jsx
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Unity, useUnityContext } from "react-unity-webgl";
 
 function UnityWebGL() {
+  const [isMobile, setIsMobile] = useState(true);
+
+  useEffect(() => {
+    const checkMobile = () => {
+      setIsMobile(/iPhone|iPad|iPod|Android/i.test(navigator.userAgent));
+    };
+
+    checkMobile();
+    window.addEventListener("resize", checkMobile);
+
+    return () => window.removeEventListener("resize", checkMobile);
+  }, []);
+
   const { unityProvider, loadingProgression } = useUnityContext({
     loaderUrl: "/WEBGLBUILDFORWEBSITE.loader.js",
     dataUrl: "/WEBGLBUILDFORWEBSITE.data",
     frameworkUrl: "/WEBGLBUILDFORWEBSITE.framework.js",
     codeUrl: "/WEBGLBUILDFORWEBSITE.wasm",
   });
+
+  if (isMobile) {
+    return (
+      <div className="unity-webgl-mobile-message">
+        <p>Unity WebGL content is not available on mobile devices.</p>
+      </div>
+    );
+  }
 
   return (
     <>
