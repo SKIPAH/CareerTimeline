455911284903aa7c24cfe4a1f786c5d5ac9a45a2
SKIPAH <juzzii.mehta@gmail.com>
2025-03-15 17:58:32 +0200

    Package fixes, few files changed to tsx

--- src/components/InstagramEmbedVideo.tsx
+++ src/components/InstagramEmbedVideo.tsx
@@ -1,12 +1,17 @@
 import { InstagramEmbed } from "react-social-media-embed";
 import React from "react";
 
-export default function InstagramEmbedVideo({ embedURL }) {
+type Props = {
+  embedURL: string;
+};
+
+const InstagramEmbedVideo = ({ embedURL }: Props) => {
   return (
     <InstagramEmbed
       url={`https://www.instagram.com/p/${embedURL}/`}
       className="instagram-embed"
     ></InstagramEmbed>
   );
-}
+};
 
+export default InstagramEmbedVideo;
---
+++ src/components/NutritionLabel.tsx
@@ -0,0 +1,120 @@
+import axios from "axios";
+import React from "react";
+import { useEffect, useState } from "react";
+
+interface PersonalBestLifts {
+  squat: number;
+  bench: number;
+  deadlift: number;
+}
+
+const NutritionLabel: React.FC = () => {
+  const kilogramToLbs: number = 2.20462;
+
+  const [personalBestLifts, setPersonalBestLifts] = useState<PersonalBestLifts>(
+    {
+      squat: 140,
+      bench: 110,
+      deadlift: 200,
+    }
+  );
+
+  useEffect(() => {
+    axios
+      .get<PersonalBestLifts>("http://localhost:3001/api/lifts")
+      .then((response) => setPersonalBestLifts(response.data))
+      .catch((error: Error) => console.error("Error fetching lifts", error));
+  }, []);
+
+  return (
+    <div className="label">
+      <header>
+        <h1 className="bold">Fitness Facts</h1>
+        <div className="divider"></div>
+        <p>8 servings per container</p>
+        <p className="bold">
+          Serving size <span>2/3 cup (55g)</span>
+        </p>
+      </header>
+      <div className="divider large"></div>
+      <div className="calories-info">
+        <div className="left-container">
+          <h2 className="bold small-text">Amount per serving</h2>
+          <p>Bodyweight</p>
+        </div>
+        <span>105kg</span>
+      </div>
+      <div className="divider medium"></div>
+      <div className="daily-value small-text">
+        <p className="bold right no-divider">231 lbs</p>
+        <div className="divider"></div>
+        <p>
+          <span>
+            <span className="bold">Total Age</span>
+          </span>{" "}
+          <span className="bold">28 years</span>
+        </p>
+
+        <p className="indent no-divider">
+          Squat{" "}
+          <span className="bold">
+            {personalBestLifts.squat}kg/
+            {Math.round(personalBestLifts.squat * kilogramToLbs)}lbs
+          </span>
+        </p>
+        <div className="divider"></div>
+        <p className="indent no-divider">
+          Bench
+          <span className="bold">
+            {personalBestLifts.bench}kg/
+            {Math.round(personalBestLifts.bench * kilogramToLbs)}lbs
+          </span>
+        </p>
+        <div className="divider"></div>
+        <p className="indent no-divider">
+          Deadlift
+          <span className="bold">
+            {personalBestLifts.deadlift}kg/
+            {Math.round(personalBestLifts.deadlift * kilogramToLbs)}lbs
+          </span>
+        </p>
+        <div className="divider"></div>
+        <p>
+          <span>
+            <span className="bold">Total Years in Gym</span>
+          </span>{" "}
+          <span className="bold">2</span>
+        </p>
+        <p>
+          <span>
+            <span className="bold">Previous sports experience</span>
+          </span>{" "}
+        </p>
+        <p className="indent no-divider">Track And Field</p>
+        <div className="divider double-indent"></div>
+        <p className="double-indent no-divider">
+          Wrestling <span className="bold"></span>
+        </p>
+        <div className="no-divider"></div>
+
+        <div className="divider large"></div>
+        <p>
+          Lean muscle mass <span>1%*</span>
+        </p>
+        <p>
+          Fat <span>99%*</span>
+        </p>
+        <p>
+          BMI <span>Too high*</span>
+        </p>
+        <p className="no-divider">
+          Mindset <span>FULL*</span>
+        </p>
+      </div>
+      <div className="divider medium"></div>
+      <p className="note">* Not actual values.</p>
+    </div>
+  );
+};
+
+export default NutritionLabel;
