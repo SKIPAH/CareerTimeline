daac339194b1160cd112c94b4945c03b336c83b4
SKIPAH <juzzii.mehta@gmail.com>
2025-04-23 00:15:14 +0300

    Firebase authentication start

--- src/Pages/LoginPage.tsx
+++ src/Pages/LoginPage.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import Signup from "../components/authentication/Signup";
+import { Container } from "react-bootstrap";
 
 function LoginPage() {
   return (
-    <div>LoginPage</div>
-  )
+    <>
+      <Container
+        className="d-flex align-items-center justify-content-center"
+        style={{ minHeight: "100vh" }}
+      >
+        <div className="w-100" style={{ maxWidth: "400px" }}>
+          <Signup />
+        </div>
+      </Container>
+    </>
+  );
 }
 
-export default LoginPage
+export default LoginPage;
